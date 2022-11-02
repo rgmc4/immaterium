faeab58dbfa16a418131eaa8f3c53a340f4eec42
Richard Clayton <rmclayton@gmail.com>
2022-11-01 21:04:26 -0400

    add navbar

---
+++ app/components/navbar.tsx
@@ -0,0 +1,75 @@
+import { Link } from "@remix-run/react";
+import { useOptionalUser } from "~/utils";
+
+type NavItemProps = {
+  route: string;
+  linkText: string;
+};
+
+function NavItem({ route, linkText }: NavItemProps) {
+  return (
+    <Link
+      to={route}
+      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-violet-700 shadow-sm hover:bg-violet-50 sm:px-8"
+    >
+      {linkText}
+    </Link>
+  );
+}
+
+export default function NavBar() {
+  const user = useOptionalUser();
+
+  return (
+    <nav className="rounded bg-gray-200 px-2 py-2.5 dark:bg-gray-900 sm:px-4">
+      <div className="container mx-auto flex flex-wrap items-center justify-between">
+        <a href="https://flowbite.com/" className="flex items-center">
+          <img src="" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            Traxis Sector
+          </span>
+        </a>
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="ml-3 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="h-6 w-6"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:text-sm md:font-medium md:dark:bg-gray-900">
+            {user ? (
+              <li>
+                <NavItem route="/login" linkText="Profile" />
+              </li>
+            ) : (
+              <>
+                <li>
+                  <NavItem route="/login" linkText="Log In" />
+                </li>
+                <li>
+                  <NavItem route="/join" linkText="Sign Up" />
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
